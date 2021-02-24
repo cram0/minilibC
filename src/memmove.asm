819019e0bd2f15ff57d8bf66ea21cfc5b2666e1e
cram <julien.augugliaro@epitech.eu>
2021-02-24 16:18:59 +0100

    Changed: copied memcpy's code to memmove

--- src/memmove.asm
+++ src/memmove.asm
@@ -5,19 +5,23 @@ SECTION .text
 
 ; rdi 1e arg dest
 ; rsi 2e arg source
-; rdx 3e arg n
+; rdx 3e arg size
+
+; Le code du memmove est essentiellement le meme que le memcpy,
+; il est cense copier les donnees ailleurs avant de remplacer les
+; donnees de la destination, mais crash si on le fait differement
 
 memmove:
     xor rcx, rcx
+    mov rax, rdi
 
 loop:
     cmp rdi, 0
     je finish
     cmp rsi, 0
     je finish
-
     cmp rdx, rcx
-    jz finish
+    je finish
 
     mov r10b, BYTE[rsi + rcx]
     mov BYTE[rdi + rcx], r10b
@@ -27,5 +31,4 @@ loop:
     jmp loop
 
 finish:
-    mov rax, rdi
     ret
